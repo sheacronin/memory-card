833138ba365cedd65260b310fa69252fedd00454
Shea Cronin <shealcronin@gmail.com>
2021-06-09 19:16:19 -0400

    Add props and data to skeleton components

--- src/App.js
+++ src/App.js
@@ -12,27 +12,48 @@ function App() {
 function MemoryGame() {
     return (
         <>
-            <ScoreBoard />
+            <ScoreBoard score={0} highScore={0} />
             <Status />
             <Board />
         </>
     );
 }
 
-function ScoreBoard() {
-    return <div>Score</div>;
+function ScoreBoard(props) {
+    const { score, highScore } = props;
+
+    return (
+        <div>
+            <div>Score: {score}</div>
+            <div>High Score: {highScore}</div>
+        </div>
+    );
 }
+
+const messages = {
+    instructions:
+        'Click on any character that you have not already selected. If you click on a character you have previously selected, you lose!',
+};
 
 function Status() {
-    return <div>Status</div>;
+    return <div>{messages.instructions}</div>;
 }
+
+const characters = [{ name: 'Emma Nelson', img: '#' }];
 
 function Board() {
-    return <Card />;
+    return <Card character={characters[0]} />;
 }
 
-function Card() {
-    return <div>Card</div>;
+function Card(props) {
+    const { character } = props;
+
+    return (
+        <button>
+            <img src={character.img} alt={character.name} />
+            {character.name}
+        </button>
+    );
 }
 
 export default App;
