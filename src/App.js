d030483181626fe1f87c77060dd6b26284958980
Shea Cronin <shealcronin@gmail.com>
2021-06-09 18:37:33 -0400

    Add placeholder components of App structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import './App.css';
 
 function App() {
-    return <div>hello world</div>;
+    return (
+        <>
+            <h1>Title</h1>
+            <MemoryGame />
+        </>
+    );
+}
+
+function MemoryGame() {
+    return (
+        <>
+            <ScoreBoard />
+            <Status />
+            <Board />
+        </>
+    );
+}
+
+function ScoreBoard() {
+    return <div>Score</div>;
+}
+
+function Status() {
+    return <div>Status</div>;
+}
+
+function Board() {
+    return <Card />;
+}
+
+function Card() {
+    return <div>Card</div>;
 }
 
 export default App;
