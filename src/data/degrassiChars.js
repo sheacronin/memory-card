5061f2f5c1008c8f89ba8c3446b942a723287933
Shea Cronin <shealcronin@gmail.com>
2021-06-17 17:11:45 -0400

    Add fn to get each char image to reduce code reuse, add Degrassi Junior High characters

--- src/data/degrassiChars.js
+++ src/data/degrassiChars.js
@@ -11,27 +11,46 @@ const charImagesContext = require.context('../i/characters', false, /\.jpeg$/);
 const charImages = importAll(charImagesContext);
 
 const CHARACTERS = [
-    { name: 'Emma Nelson', img: charImages.emma, series: ['DTNG'] },
-    { name: 'Manny Santos', img: charImages.manny, series: ['DTNG'] },
-    { name: 'Liberty Van Zandt', img: charImages.liberty, series: ['DTNG'] },
-    { name: 'Jimmy Brooks', img: charImages.jimmy, series: ['DTNG'] },
-    { name: 'Terri MacGregor', img: charImages.terri, series: ['DTNG'] },
-    { name: 'Ashley Kerwin', img: charImages.ashley, series: ['DTNG'] },
-    { name: 'Toby Isaacs', img: charImages.toby, series: ['DTNG'] },
-    { name: 'Spinner Mason', img: charImages.spinner, series: ['DTNG'] },
-    { name: 'J.T. Yorke', img: charImages.jt, series: ['DTNG'] },
-    { name: 'Paige Michalchuk', img: charImages.paige, series: ['DTNG'] },
-    { name: 'Sean Cameron', img: charImages.sean, series: ['DTNG'] },
-    { name: 'Craig Manning', img: charImages.craig, series: ['DTNG'] },
-    { name: 'Hazel Aden', img: charImages.hazel, series: ['DTNG'] },
-    { name: 'Marco Del Rossi', img: charImages.marco, series: ['DTNG'] },
-    { name: 'Ellie Nash', img: charImages.ellie, series: ['DTNG'] },
-    { name: 'Joey Jeremiah', img: '#', series: ['DJH'] },
-    { name: 'Caitlin Ryan', img: '#', series: ['DJH'] },
-    { name: 'Snake', img: '#', series: ['DJH'] },
-    { name: 'Lucy Fernandez', img: '#', series: ['DJH'] },
-    { name: 'Wheels', img: '#', series: ['DJH'] },
-    { name: 'Stephanie Kaye', img: '#', series: ['DJH'] },
+    { name: 'Emma Nelson', series: ['DTNG'] },
+    { name: 'Manny Santos', series: ['DTNG'] },
+    { name: 'Liberty Van Zandt', series: ['DTNG'] },
+    { name: 'Jimmy Brooks', series: ['DTNG'] },
+    { name: 'Terri MacGregor', series: ['DTNG'] },
+    { name: 'Ashley Kerwin', series: ['DTNG'] },
+    { name: 'Toby Isaacs', series: ['DTNG'] },
+    { name: 'Spinner Mason', series: ['DTNG'] },
+    { name: 'J.T. Yorke', series: ['DTNG'] },
+    { name: 'Paige Michalchuk', series: ['DTNG'] },
+    { name: 'Sean Cameron', series: ['DTNG'] },
+    { name: 'Craig Manning', series: ['DTNG'] },
+    { name: 'Hazel Aden', series: ['DTNG'] },
+    { name: 'Marco Del Rossi', series: ['DTNG'] },
+    { name: 'Ellie Nash', series: ['DTNG'] },
+    { name: 'Joey Jeremiah', series: ['DJH'] },
+    { name: 'Caitlin Ryan', series: ['DJH'] },
+    { name: 'Snake', series: ['DJH'] },
+    { name: 'Lucy Fernandez', series: ['DJH'] },
+    { name: 'Wheels', series: ['DJH'] },
+    { name: 'Stephanie Kaye', series: ['DJH'] },
+    { name: 'Arthur Kobalewscuy', series: ['DJH'] },
+    { name: 'Yick Yu', series: ['DJH'] },
+    { name: 'Shane McKay', series: ['DJH'] },
+    { name: 'Erica Farrell', series: ['DJH'] },
+    { name: 'Heather Farrell', series: ['DJH'] },
+    { name: 'Melanie Brodie', series: ['DJH'] },
+    { name: 'Kathleen Mead', series: ['DJH'] },
+    { name: 'Spike', series: ['DJH'] },
+    { name: 'L.D. Delacorte', series: ['DJH'] },
 ];
+
+CHARACTERS.forEach((char) => {
+    const spaceIndex = char.name.indexOf(' ');
+    const firstName =
+        spaceIndex !== -1
+            ? char.name.slice(0, spaceIndex).toLowerCase()
+            : char.name.toLowerCase();
+    console.log(firstName);
+    char.img = charImages[firstName];
+});
 
 export default CHARACTERS;
