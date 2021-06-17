c7ded94474a256f9bdbb4c8ad723a7ddff688158
Shea Cronin <shealcronin@gmail.com>
2021-06-17 14:18:49 -0400

    Add async animation to clicking Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,33 @@ import '../styles/Card.css';
 function Card(props) {
     const { character, handleCardClick } = props;
 
+    async function animateCardClick(card) {
+        card.classList.add('click-card');
+        await Promise.all(card.getAnimations().map((anim) => anim.finished));
+        card.classList.remove('click-card');
+        return new Promise((resolve, reject) => {
+            if (!card.classList.contains('click-card')) {
+                resolve('The animation worked!');
+            } else {
+                reject('Something went wrong.');
+            }
+        });
+    }
+
     return (
-        <button className="card" onClick={() => handleCardClick(character)}>
+        <button
+            className="card"
+            onClick={(e) => {
+                animateCardClick(e.currentTarget)
+                    .then((message) => {
+                        console.log(message);
+                        handleCardClick(character);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }}
+        >
             <img src={character.img} alt={character.name} />
             <p>{character.name.toLowerCase()}</p>
         </button>
